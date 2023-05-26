66fd73098e38cf2af0fe19eca778c56e4ac47073
Miguel Natan Tomaz <miguelnatantomaz@hotmail.com>
2023-05-25 22:00:36 -0300

    feat: atividade somativa 1, estrutura de Dados em Java: Lista

--- EstudosJava/src/Lista/Lista.java
+++ EstudosJava/src/Lista/Lista.java
@@ -9,7 +9,7 @@ public class Lista {
 	private No primeiro;
 	
 	public Lista() {
-		primeiro = null;	
+		primeiro = null;
 	}
 	
 	public boolean vazio() {
@@ -41,11 +41,73 @@ public class Lista {
 		auxiliar.setProx(novo);
 	}
 	
+	public No getPrimeiro() {
+		return primeiro.getProx().getProx();
+	}
+	
+	public void removePrimeiro() {
+		if(this.vazio()) return;
+		
+		No auxiliar = primeiro;
+		
+		auxiliar = auxiliar.getProx();
+		auxiliar.setInfo(auxiliar.getInfo());
+		
+		primeiro = auxiliar;
+	}
+	
+	public void removeUltimo() {
+		if(this.vazio()) return;
+		
+		No penultimo = primeiro;
+		No auxiliar = primeiro;
+		
+		while(auxiliar != null && auxiliar.getProx() != null) {
+			penultimo = auxiliar;
+			auxiliar = auxiliar.getProx();
+		}
+		
+		penultimo.setProx(null);
+	}
+	
+	public void insereDepois(No no, int info) {
+		if(this.vazio()) return;
+		
+		if(no == null) {
+			insereUltimo(info);
+			return;
+		}
+		
+		No novo = new No();	
+		novo.setInfo(info);
+		novo.setProx(no.getProx());
+		no.setProx(novo);
+	}
+	
+	public void remove(No no) {
+		if(this.vazio() || no == null) return;
+		
+		if(no == primeiro) {
+			removePrimeiro();
+			return;
+		}
+		
+		No auxiliar = primeiro;
+		
+		while(auxiliar.getProx() != no) {
+			auxiliar = auxiliar.getProx();
+		}
+		
+		No noRemovido = auxiliar.getProx();
+		
+		auxiliar.setProx(noRemovido.getProx());
+	}
+	
 	public void mostrar() {
 		No auxiliar = primeiro;
 		while(auxiliar != null) {
 			System.out.print(" | " + auxiliar.getInfo());
-			auxiliar = auxiliar.getProx();		
+			auxiliar = auxiliar.getProx();
 		}
 		
 	}
@@ -56,9 +118,45 @@ public class Lista {
 		l.insereUltimo(20);
 		l.insereUltimo(30);
 		
+		l.mostrar();
+		
+		System.out.print("| \n antes inserePrimeiro \n");
+		
 		l.inserePrimeiro(0);
 		
+		l.mostrar();
+		
+		System.out.print("| \n antes de insereUltimo \n");
+		
 		l.insereUltimo(250);
+		
+		l.mostrar();
+		
+		System.out.print("| \n antes de remover primeiro \n");
+		
+		l.removePrimeiro();
+		
+		l.mostrar();
+		
+		System.out.print("| \n antes de remover ultimo \n");
+		
+		l.removeUltimo();
+		
+		l.mostrar();
+		
+		System.out.print("| \n antes de remove() primeiro \n");
+		
+		No primeiroNo = l.getPrimeiro();
+		
+		l.remove(primeiroNo);
+				
+		l.mostrar();
+		
+		System.out.print("| \n Antes do insereDepois \n");
+		
+		No primeiroNoAgora = l.getPrimeiro();
+		
+		l.insereDepois(primeiroNoAgora, 555);
 		
 		l.mostrar();
 		
